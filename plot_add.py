13119e7c4399170092ec98ae97c1308e75829772
Gerard <gerard.kennedy@anu.edu.au>
2020-08-07 08:51:50 +1000

    before merge

--- plot_add.py
+++ plot_add.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from mpl_toolkits import mplot3d
 import numpy as np
 
-add_list_list = np.load("/home/gerard/myPvnet/pvnet/change_unit_vectors/add_list.npy")
+add_list_list = np.load("/home/gerard/GIT/pvnet/change_unit_vectors/add_list.npy")
 epoch_count = len(add_list_list)
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = plt.axes(projection='3d')
 print('add_list: ',add_list_list)
 X = np.arange(0,len(add_list_list[0]),1)
 Z = np.arange(0,epoch_count,1)
-X,Z = np.meshgrid(X,Z)
 Y = add_list_list
+X,Z = np.meshgrid(X,Z)
+ax.plot_surface(X, Y, Z, cmap='viridis')
 ax.set_xlabel("iterations")
 ax.set_ylabel("add")
 ax.set_zlabel('epoch')
-surf = ax.plot_surface(X, Y, Z,
-                linewidth=0, antialiased=False)
 plt.show()
 plt.savefig('add_list.png')
